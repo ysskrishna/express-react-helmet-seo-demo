a18b15e46764cf8260d90cacec53c04c591b15aa
ysskrishna <sivasaikrishnassk@gmail.com>
2025-02-23 15:02:37 +0530

    Siva | Setup concurrently for running frontend and backend simultaneously

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch('/api/health')
+      .then(response => response.json())
+      .then(data => console.log("health check", data))
+      .catch(error => console.error('Error:', error))
+  }, []);
 
   return (
     <>
