842d7597470a0f45dea7636c026f16db567bd3a1
ysskrishna <sivasaikrishnassk@gmail.com>
2025-03-09 19:52:54 +0530

    Siva | Add copyfiles for JSON file handling in production build

--- server/core/dbutils.ts
+++ server/core/dbutils.ts
@@ -2,12 +2,12 @@ import { JSONFilePreset } from "lowdb/node";
 import path from "path";
 import { Database } from "../types";
 import { Low } from "lowdb";
-import { getDirname } from "./pathutils";
+import { getStaticFilePath } from "./pathutils";
 
 export let db: Low<Database>;
 
 export const createConnection = async () => {
-    const dbPathAndFileName = path.join(getDirname(import.meta.url), "db.json");
+    const dbPathAndFileName = getStaticFilePath(import.meta.url, "db.json");
     console.log("Creating connection to database...", dbPathAndFileName);
 
     db = await JSONFilePreset<Database>(dbPathAndFileName, { recipes: [] });
--- server/core/pathutils.ts
+++ server/core/pathutils.ts
@@ -1,10 +1,19 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-export const getDirname = (metaUrl: string) => {
+export const getDirname = (metaUrl: string): string => {
   return path.dirname(fileURLToPath(metaUrl));
 };
 
-export const getFilename = (metaUrl: string) => {
-  return fileURLToPath(metaUrl);
+export const getStaticFilePath = (metaUrl: string, fileName: string): string => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const currentDir = getDirname(metaUrl);
+  
+  if (!isProd) {
+    return path.join(currentDir, fileName);
+  }
+
+  // In production, all assets are flattened in the dist directory 
+  // TODO: Explore fix for multiple JSON files with the same name in different directories (like server/core/config.json and server/services/config.json)
+  return path.join(process.cwd(), 'dist', fileName);
 };
